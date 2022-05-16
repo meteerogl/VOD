2be145ef5ece2128eb612693e05f79a50466f986
meteeroglu <mete@evreka.co>
2022-05-16 23:55:08 +0300

    create views(ModelViewSet)

--- content/content/views.py
+++ content/content/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from content.models import Content
+from content.serializers import ContentSerializer
+
+
+class ContentViewSet(viewsets.ViewSet):
+    def list(self, request):
+        contents = Content.objects.all()
+        serializer = ContentSerializer(contents, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def destroy(self, request):
+        pass
