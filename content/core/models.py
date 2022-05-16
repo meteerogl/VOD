bfcb5142fc4b1e50546497ccbf8b164f6d017f2f
meteeroglu <mete@evreka.co>
2022-05-16 23:16:39 +0300

    fix "id_prefix"

--- content/core/models.py
+++ content/core/models.py
@@ -21,3 +21,5 @@ class Content(BaseModel):
     name        = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     catalog_id  = models.IntegerField(default=0)
+
+    id_prefix   = "content"
