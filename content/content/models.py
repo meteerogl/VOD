b4d52422d0edd2001f42308521441befa1c6d16d
meteeroglu <mete@evreka.co>
2022-05-17 00:30:04 +0300

    #commit:content_api create and retrieve view

--- content/content/models.py
+++ content/content/models.py
@@ -5,3 +5,5 @@ class Content(BaseModel):
     name        = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     catalog_id  = models.IntegerField(default=0)
+
+    id_prefix = "content"
--- content/content/views.py
+++ content/content/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from content.models import Content
@@ -13,10 +13,15 @@ class ContentViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        pass
+        serializer = ContentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
-    def retrieve(self, request):
-        pass
+    def retrieve(self, request, pk=None):
+        content = Content.objects.get(id=pk)
+        serializer = ContentSerializer(content)
+        return Response(serializer.data)
 
     def update(self, request):
         pass
