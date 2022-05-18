6380941e0f43fb5b05767e346e9c1cea33de464a
meteeroglu <mete@evreka.co>
2022-05-19 00:44:16 +0300

    #commit: create catalog service models

--- catalog/main.py
+++ catalog/main.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
+# -----------> Flask App
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/catalog'
+CORS(app)  # For frontend
+
+# -----------> Database Models
+db = SQLAlchemy(app)
+
+class Catalog(db.Model):
+    id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name        = db.Column(db.String(100))
+    description = db.Column(db.String(255))
+
 
 @app.route('/')
 def index():
