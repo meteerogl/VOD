b002ae19812ac7c3b7f527b3e33ff95c38726c79
meteeroglu <mete@evreka.co>
2022-05-20 01:02:40 +0300

    #commit: implement redis on content(docker-compose file, requirements, some codes)

--- content/content/views.py
+++ content/content/views.py
@@ -4,18 +4,26 @@ from rest_framework.response import Response
 
 from content.models import Content
 from content.serializers import ContentSerializer
+from django.core.cache import cache as redis_cache
 
 
+# TODO: Write helper functions or decarator for update redis
 class ContentViewSet(viewsets.ViewSet):
     def list(self, request):
-        contents = Content.objects.all()
-        serializer = ContentSerializer(contents, many=True)
-        return Response(serializer.data)
+        redis_response = redis_cache.get('content_list')
+        if redis_response is None:
+            # Update Redis
+            redis_cache.set('content_list', ContentSerializer(Content.objects.all(), many=True).data)
+            return Response(redis_cache.get('content_list'))
+
+        return Response(redis_response)
 
     def create(self, request):
         serializer = ContentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        # Update Redis
+        redis_cache.set('content_list', ContentSerializer(Content.objects.all(), many=True).data)
         return Response(serializer.data, status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -28,10 +36,14 @@ class ContentViewSet(viewsets.ViewSet):
         serializer = ContentSerializer(instance=content, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        # Update Redis
+        redis_cache.set('content_list', ContentSerializer(Content.objects.all(), many=True).data)
         return Response(serializer.data, status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk):
         content = Content.objects.get(id=pk)
         content.delete()
+        # Update Redis
+        redis_cache.set('content_list', ContentSerializer(Content.objects.all(), many=True).data)
         return Response(status.HTTP_204_NO_CONTENT)
 
